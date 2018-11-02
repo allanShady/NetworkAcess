80444fc50ee25f33cd3d9f868614ce3b268087d7
Allan Camilo <acamilo.ag@gmail.com>
2018-11-02 10:03:03 +0200

    First  commit. this code can read a specif shared folder

---
+++ NetworkAcessSharedFiles/Helper/FileProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NetworkAcessSharedFiles.Helpers
+{
+    public static class FileProcessor
+    {
+        /// <summary>
+        /// Load the contents of the file. 
+        /// </summary>
+        /// <param name="file">File to load the contents.</param>
+        /// <returns>contents as list of string.</returns>
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(file).ToList();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>  
+        /// <param name="fullFilePath"> e.g: \\OFFICEMANAGER\PRISERVER </param>
+        /// <param name="content">The list of strings to be write on the file</param>
+        /// <returns>The list of string of all contents</returns>
+        public static List<string> WriteFile(this string fullFilePath, string [] content)
+        {
+            if (!File.Exists(fullFilePath))
+            { 
+                File.WriteAllLines(fullFilePath, content);
+            }
+            return File.ReadAllLines(fullFilePath).ToList();
+        }
+    }
+}
--- NetworkAcessSharedFiles/Program.cs
+++ NetworkAcessSharedFiles/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NetworkAcessSharedFiles.Helpers;
+using System;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +9,22 @@ namespace NetworkAcessSharedFiles
     {
         static void Main(string[] args)
         {
+            var networkPath = @"\\Desktop-m32m6di\priserver";
+            var credentials = new System.Net.NetworkCredential("levi.camoes", "levi2018");
+
+            using (new NetworkConnection(networkPath, credentials))
+            {
+                StringBuilder fileDir = new StringBuilder();
+
+                fileDir.Append(@"\\Desktop-m32m6di\priserver");
+                fileDir.Append(@"\");
+                fileDir.Append("Peso.txt");
+                
+                var fileContet = FileProcessor.LoadFile(fileDir.ToString());
+                Console.WriteLine($"From: {fileDir}\n{fileContet.FirstOrDefault()}");
+                //var fileList = Directory.GetFiles(networkPath);
+            }
+            Console.ReadLine();
         }
     }
 }
